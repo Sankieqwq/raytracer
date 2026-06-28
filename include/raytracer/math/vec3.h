87a5c15ea12d88db7e498c408c336afbd902b2ac
hudaijin <hudaijin@kuaishou.com>
2026-06-28 19:43:18 +0800

    Stage 8 task 1: add Vec2 type for UV coordinates

--- include/raytracer/math/vec3.h
+++ include/raytracer/math/vec3.h
@@ -81,4 +81,20 @@ inline Color gamma2(const Color& c) {
     return Color(std::sqrt(c.x), std::sqrt(c.y), std::sqrt(c.z));
 }
 
+// --- Vec2 (for UV coordinates) ---
+class Vec2 {
+public:
+    double x, y;
+
+    Vec2() : x(0), y(0) {}
+    Vec2(double x, double y) : x(x), y(y) {}
+
+    Vec2& operator+=(const Vec2& o) { x += o.x; y += o.y; return *this; }
+    Vec2& operator*=(double t) { x *= t; y *= t; return *this; }
+};
+
+inline Vec2 operator+(const Vec2& a, const Vec2& b) { return Vec2(a.x+b.x, a.y+b.y); }
+inline Vec2 operator*(double t, const Vec2& a) { return Vec2(t*a.x, t*a.y); }
+inline Vec2 operator*(const Vec2& a, double t) { return t * a; }
+
 #endif
